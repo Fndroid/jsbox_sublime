9d17017c078ce76d5cb8b44af613ca8fa4276d14
Fndroid <1603808949@qq.com>
2018-03-11 18:03:47 +0800

    Update jsbox.py

--- Jsbox/jsbox.py
+++ Jsbox/jsbox.py
@@ -12,45 +12,44 @@ SETTINGS_FILE = 'Jsbox.sublime-settings'
 
 class JsboxsyncfileCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		try:
-			settings = sublime.load_settings(SETTINGS_FILE)
-			host = settings.get('host')
+		settings = sublime.load_settings(SETTINGS_FILE)
+		host = settings.get('host')
 
-			file_name = os.path.abspath(self.view.file_name())
-			directory = os.path.dirname(file_name)
+		file_name = os.path.abspath(self.view.file_name())
+		directory = os.path.dirname(file_name)
 
-			is_package = False
-			# print(directory)
-			while directory:
-				needed_files = ['assets', 'scripts', 'strings', 'config.json', 'main.js']
-				directory_files = os.listdir(directory)
-				legal_dir = all([i in directory_files for i in needed_files])
-				if legal_dir or os.path.dirname(directory) == directory:
-					is_package = legal_dir
-					break
-				directory = os.path.dirname(directory)
+		is_package = False
+		# print(directory)
+		while directory:
+			print(directory)
+			needed_files = ['assets', 'scripts', 'strings', 'config.json', 'main.js']
+			directory_files = os.listdir(directory)
+			legal_dir = all([i in directory_files for i in needed_files])
+			if legal_dir or os.path.dirname(directory) == directory:
+				is_package = legal_dir
+				break
+			directory = os.path.dirname(directory)
 
-			print(is_package)
+		print("是否安装包:{}".format(is_package))
 
-			if is_package:
-				output_path = os.path.join(directory, '.output')
-				if not os.path.exists(output_path):
-					os.mkdir(output_path)
-				zipfile_name = os.path.basename(directory)
-				shutil.make_archive(zipfile_name, 'zip', directory)
-				file_name = os.path.join(directory, '.output', zipfile_name + '.zip')
+		if is_package:
+			output_path = os.path.join(directory, '.output')
+			if not os.path.exists(output_path):
+				os.mkdir(output_path)
+			zipfile_name = os.path.basename(directory)
+			print("目录名称: {}, 文件名: {}".format(output_path, directory))
+			file_name = shutil.make_archive(os.path.join(output_path, "shuangpin"), 'zip', root_dir=directory, base_dir=None)
+			print("压缩文件名: {}".format(file_name))
+			# file_name = name
 
-			def upload():
-				url = 'http://{}/upload'.format(host)
-				files = {'files[]': open(file_name, 'rb')}
-				r = requests.post(url, files=files)
-				print(r.status_code)
-				if r.status_code != 200:
-					sublime.error_message('fail')
-			sublime.set_timeout_async(upload, 0)
-		except: 
-			print('error')
-			sublime.error_message('fail')
+		def upload():
+			url = 'http://{}/upload'.format(host)
+			files = {'files[]': open(file_name, 'rb')}
+			r = requests.post(url, files=files)
+			print(r.status_code)
+			if r.status_code != 200:
+				sublime.error_message('fail')
+		sublime.set_timeout_async(upload, 0)
 
 class JsboxdownloadfileCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -62,7 +61,7 @@ class JsboxdownloadfileCommand(sublime_plugin.TextCommand):
 		resp = requests.get(url)
 		resp.encoding = 'utf-8'
 		scripts = resp.json()
-		print(map(lambda x: x['name'], resp.json()))
+		# print(map(lambda x: x['name'], resp.json()))
 		def load(x):
 			downloadurl = 'http://{}/download?path={}'.format(host, scripts[x]['path'])
 			resp2 = requests.get(downloadurl)
@@ -76,7 +75,7 @@ class JsboxdownloadfileCommand(sublime_plugin.TextCommand):
 class JsboxEventListener(sublime_plugin.EventListener):
 	def on_post_save_async(self, view):
 		filename = view.file_name()
-		print(os.path.basename(filename))
+		# print(os.path.basename(filename))
 		if filename.endswith('.js') or filename.endswith('.json'):
-			print('文件以js或json结尾')
+			# print('文件以js或json结尾')
 			view.run_command('jsboxsyncfile')
