d36acd42079f5e8fefbd9e4f25b00e51e9eec9a7
Fndroid <1603808949@qq.com>
2018-03-11 14:01:10 +0800

    add plugin files

---
+++ Jsbox/jsbox.py
@@ -0,0 +1,82 @@
+import json
+import sublime
+import sublime_plugin
+import urllib
+import os
+import requests
+import re
+import shutil
+
+SETTINGS_FILE = 'Jsbox.sublime-settings'
+
+
+class JsboxsyncfileCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		try:
+			settings = sublime.load_settings(SETTINGS_FILE)
+			host = settings.get('host')
+
+			file_name = os.path.abspath(self.view.file_name())
+			directory = os.path.dirname(file_name)
+
+			is_package = False
+			# print(directory)
+			while directory:
+				needed_files = ['assets', 'scripts', 'strings', 'config.json', 'main.js']
+				directory_files = os.listdir(directory)
+				legal_dir = all([i in directory_files for i in needed_files])
+				if legal_dir or os.path.dirname(directory) == directory:
+					is_package = legal_dir
+					break
+				directory = os.path.dirname(directory)
+
+			print(is_package)
+
+			if is_package:
+				output_path = os.path.join(directory, '.output')
+				if not os.path.exists(output_path):
+					os.mkdir(output_path)
+				zipfile_name = os.path.basename(directory)
+				shutil.make_archive(zipfile_name, 'zip', directory)
+				file_name = os.path.join(directory, '.output', zipfile_name + '.zip')
+
+			def upload():
+				url = 'http://{}/upload'.format(host)
+				files = {'files[]': open(file_name, 'rb')}
+				r = requests.post(url, files=files)
+				print(r.status_code)
+				if r.status_code != 200:
+					sublime.error_message('fail')
+			sublime.set_timeout_async(upload, 0)
+		except: 
+			print('error')
+			sublime.error_message('fail')
+
+class JsboxdownloadfileCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		window = sublime.active_window()
+		new_file = window.new_file()
+		settings = sublime.load_settings(SETTINGS_FILE)
+		host = settings.get('host')
+		url = 'http://{}/list?path=/'.format(host)
+		resp = requests.get(url)
+		resp.encoding = 'utf-8'
+		scripts = resp.json()
+		print(map(lambda x: x['name'], resp.json()))
+		def load(x):
+			downloadurl = 'http://{}/download?path={}'.format(host, scripts[x]['path'])
+			resp2 = requests.get(downloadurl)
+			resp2.encoding = 'utf-8'
+			new_file.insert(edit, 0, resp2.text)
+			new_file.set_name(scripts[x]['name'])
+			new_file.set_line_endings('Unix')
+		new_file.show_popup_menu(list(map(lambda x: x['name'], scripts)), load)
+
+
+class JsboxEventListener(sublime_plugin.EventListener):
+	def on_post_save_async(self, view):
+		filename = view.file_name()
+		print(os.path.basename(filename))
+		if filename.endswith('.js') or filename.endswith('.json'):
+			print('文件以js或json结尾')
+			view.run_command('jsboxsyncfile')
